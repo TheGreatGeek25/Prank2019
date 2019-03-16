dcbc60408920338b76a6d4ff9c30e1145075e543
TheGreatGeek25 <TheGreatGeek25@users.noreply.github.com>
2019-03-15 22:59:49 -0500

    Add license info and basic setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='COBOML',
+    version='1.0.0',
+    packages=['coboml'],
+    package_dir={'': 'src'},
+    package_data={
+        '': ['LICENSE']
+    },
+
+    url='thegreatgeek25.github.io/COBOML',
+    license='Apache-2.0'
+)
