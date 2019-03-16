a165ebfbe1f3f8ffd3352051486685542f02efb7
TheGreatGeek25 <TheGreatGeek25@users.noreply.github.com>
2019-03-15 22:23:14 -0500

    Add __main__.py

--- src/coboml/__init__.py
+++ src/coboml/__init__.py
@@ -1,3 +1,11 @@
 import coboml.parser as parser
 import coboml.compiler as compiler
 import coboml.util as util
+
+
+def compile_file(source_file, output_file):  # Side effects :(
+    output_file.write(compiler.compile_COBOML(source_file.read()))
+
+
+def compile_str(source: str) -> str:
+    return compiler.compile_COBOML(source)
