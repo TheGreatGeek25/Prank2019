f89c5220b5887c465188049afb3464378bd32a92
TheGreatGeek25 <TheGreatGeek25@users.noreply.github.com>
2019-04-01 22:00:39 -0500

    Increase version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PlainEnglishMarkupLanguage',
-    version='1.0.0',
+    version='1.1.0',
     packages=['plainenglishmarkuplanguage'],
     package_dir={'': 'src'},
     package_data={
